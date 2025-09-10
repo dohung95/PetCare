ee93ad37a6e0b8df4adbcbe0d40583dc3adaaacb
lehuy240797 <lehuy240797@gmail.com>
2025-09-10 13:39:40 +0700

    update navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,11 +10,21 @@ function App() {
         <Routes>
           <Route path="/" element={<h1>Home Page</h1>} />
           <Route path="/service" element={<h1>Service Page</h1>} />
+          <Route path="/service/store" element={
+            <div>
+              <h1>Store Page</h1>
+              <ul>
+                <li><a href="/service/dog-products">Dog Products</a></li>
+                <li><a href="/service/cat-products">Cat Products</a></li>
+              </ul>
+            </div>
+          } />
+          <Route path="/service/dog-products" element={<h1>Dog Products Page</h1>} />
+          <Route path="/service/cat-products" element={<h1>Cat Products Page</h1>} />
           <Route path="/about" element={<h1>About Page</h1>} />
           <Route path="/contact" element={<h1>Contact Page</h1>} />
           <Route path="/job" element={<h1>Job Page</h1>} />
           <Route path="/login" element={<h1>Login Page</h1>} />
-          <Route path="/login1" element={<h1>Login Page</h1>} />
         </Routes>
       </div>
     </Router>
