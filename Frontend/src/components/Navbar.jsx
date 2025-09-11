702111ebb7f45cee344af0e06c1ffcda9e3c0198
PVHuy95 <phuvinhhuy1995@gmail.com>
2025-09-11 12:35:17 +0700

    fix file Thu

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -38,7 +38,14 @@ const Navbar = () => {
               <div className="menu-item">
                 <Link to="/contact" className="nav-link">Contact</Link>
               </div>
-              <div 
+
+               <div className="menu-item">
+                <Link to="/adoption" className="nav-link">adoption</Link>
+              </div>
+             
+
+              <div
+
                 className="menu-item dropdown"
                 onMouseEnter={() => setJobOpen(true)}
                 onMouseLeave={() => setJobOpen(false)}
